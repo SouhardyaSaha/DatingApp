8fdb7958bb0d7810dfe8b42f2893a0d76ea821cd
SouhardyaSaha <souhardyasaha98@gmail.com>
2022-01-19 14:42:53 +0600

    Initial Commit For Client

--- backend/Startup.cs
+++ backend/Startup.cs
@@ -38,6 +38,7 @@ namespace backend
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv5", Version = "v1" });
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +55,8 @@ namespace backend
 
             app.UseRouting();
 
+            app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+            
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
