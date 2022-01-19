ecd8b545be8dba018e64f65bafddcd9f7c672994
SouhardyaSaha <souhardyasaha98@gmail.com>
2022-01-19 13:45:01 +0600

    UsersController

--- backend/Controllers/UsersController.cs
+++ backend/Controllers/UsersController.cs
@@ -1,12 +1,30 @@
+using backend.Data;
+using backend.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace backend.Controllers;
 
-public class UsersController : Controller
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController : ControllerBase
 {
-    // GET
-    public IActionResult Index()
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
     {
-        return View();
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        return await _context.Users.ToListAsync();
+    }
+    
+    [HttpGet("{id}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        return await _context.Users.FindAsync(id);
     }
 }
