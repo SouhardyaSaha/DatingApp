a82b9c54dac050f9912aae12dd3c86171b55f0e5
SouhardyaSaha <souhardyasaha98@gmail.com>
2022-01-19 20:47:21 +0600

    Account register login

---
+++ backend/DTOs/RegisterDto.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace backend.DTOs;
+
+public class RegisterDto
+{
+    [Required]
+    public string UserName { get; set; }
+
+    [Required]
+    public string Password { get; set; }
+}
