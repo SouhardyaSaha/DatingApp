dc76dda32b06878a7df9f22019a2147defbd309a
SouhardyaSaha <souhardyasaha98@gmail.com>
2022-01-28 20:45:23 +0600

    error handling backend

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from "./pages/auth/services/auth.service";
-import { MatSnackBar } from "@angular/material/snack-bar";
-import { NotificationService } from "./shared/services/notification.service";
 
 @Component({
   selector: 'app-root',
@@ -11,37 +9,10 @@ import { NotificationService } from "./shared/services/notification.service";
 export class AppComponent implements OnInit {
   title = 'DatingApp';
 
-  constructor(
-    private authService: AuthService,
-    public notificationService: NotificationService,
-    private _snackBar: MatSnackBar
-  ) {
+  constructor(private authService: AuthService) {
   }
 
   ngOnInit(): void {
     this.authService.autoLogin()
-  }
-
-  openSnackBar(message: string, action: string, className: string) {
-    this._snackBar.open(message, action, {
-      duration: 0,
-      panelClass: [className]
-    });
-  }
-
-  showAlert() {
-    this.notificationService.alert("an alert", "notice", () => {
-      this.notificationService.success("alert poked");
-    });
-  }
-
-  showConfirm() {
-    this.notificationService.confirmation("it will be gone forever", () => {
-        this.notificationService.success("confirm poked");
-      },
-      'Are you sure?',
-      () => {
-        this.notificationService.error("confirm canceled");
-      });
   }
 }
