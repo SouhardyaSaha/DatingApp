a0d972a15676f219407506bcac7554a7568b357f
SouhardyaSaha <souhardyasaha98@gmail.com>
2022-01-31 15:33:09 +0600

    extended user entity and user seeding

---
+++ backend/Data/Seed.cs
@@ -0,0 +1,31 @@
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using backend.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace backend.Data;
+
+public class Seed
+{
+    public static async Task SeedUsers(DataContext context)
+    {
+        if (await context.Users.AnyAsync()) return;
+
+        var userData = await System.IO.File.ReadAllTextAsync("Data/UserSeedData.json");
+        var users = JsonSerializer.Deserialize<List<AppUser>>(userData);
+        Console.Write(users);
+        foreach (var user in users)
+        {
+            using var hmac = new HMACSHA512();
+
+            user.UserName = user.UserName.ToLower();
+            user.Password = hmac.ComputeHash(Encoding.UTF8.GetBytes("123123"));
+            user.PasswordSalt = hmac.Key;
+
+            context.Users.Add(user);
+        }
+
+        await context.SaveChangesAsync();
+    }
+}
